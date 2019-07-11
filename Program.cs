0b9118996306e166d25606652b9375c3c52612b7
Rob Orndoff <rorndoff@gmail.com>
2019-07-11 13:00:24 -0400

    second commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Optional_Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
